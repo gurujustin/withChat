45f2aac39f521a4e8832d3d97301f8e84cf2cbcc
Ivan <davidayewah7@gmail.com>
2022-11-04 15:38:23 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,19 +7,20 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const withTM = require('next-transpile-modules')(['@pancakeswap/uikit', '@pancakeswap/sdk'])
 
 const sentryWebpackPluginOptions =
-  process.env.VERCEL_ENV === 'production'
-    ? {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
-        silent: false, // Logging when deploying to check if there is any problem
-        validate: true,
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      }
-    : {
+  // process.env.VERCEL_ENV === 'production'
+    // ? {
+    //     // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    //     // the following options are set automatically, and overriding them is not
+    //     // recommended:
+    //     //   release, url, org, project, authToken, configFile, stripPrefix,
+    //     //   urlPrefix, include, ignore
+    //     silent: false, // Logging when deploying to check if there is any problem
+    //     validate: true,
+    //     // For all available options, see:
+    //     // https://github.com/getsentry/sentry-webpack-plugin#options.
+    //   }
+    // : 
+    {
         silent: true, // Suppresses all logs
         dryRun: !process.env.SENTRY_AUTH_TOKEN,
       }
