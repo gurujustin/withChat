cf8ff1bc4d11e9e67b2273ed1691e2e28b7ee08f
Ivan <davidayewah7@gmail.com>
2022-11-05 16:37:26 +1000

    udpate

--- src/components/ChatLayout/index.tsx
+++ src/components/ChatLayout/index.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat, Channel, ChannelHeader, ChannelList, LoadingIndicator, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-import { useClient } from '../../hooks/useClient';
+import { fetchTokenFromApi, useClient } from '../../hooks/useClient';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 import { useWeb3React } from '../../../packages/wagmi/src/useWeb3React';
+import { useTheme } from 'styled-components';
+import { _nameprepTableB2 } from '@ethersproject/strings/lib/idna';
 
-const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoib2xkLXZvaWNlLTIifQ.ZabDZuOVXi0EIKiwSTbJgBcvMzahocxU-Rw4ZCZ0RTo';
 
 
 
@@ -16,25 +17,30 @@ const sort = { last_message_at: -1 };
 
 const ChatComponent = () => {
   const { account } = useWeb3React()
+  const theme = useTheme()
+  const chatTheme = theme.isDark ? "str-chat__theme-dark" : "str-chat__theme-light"
+  const name = account ? account : "old-voice-2"
   const user = {
-    id: 'old-voice-2',
-    name: 'old-voice-2',
-    image: 'https://getstream.io/random_png/?id=old-voice-2&name=old-voice-2',
+    id: name,
+    name: name,
+    image: `https://getstream.io/random_png/?id=${name}&name=${name}`,
   };
-  const chatClient = useClient({ apiKey: 'b45d7ce7cxdr', userData: user, tokenOrProvider: userToken });
 
+  const userToken = fetchTokenFromApi(name)
+  const chatClient = useClient({ apiKey: 'sy67pe8nsjze', userData: user, tokenOrProvider: userToken });
   if (!chatClient) {
     return <LoadingIndicator />;
   }
 
+
   return (
-    <Chat client={chatClient} theme='str-chat__theme-light'>
+    <Chat client={chatClient} theme={chatTheme}>
       <ChannelList filters={filters} />
       <Channel>
         <Window>
           <ChannelHeader />
           <MessageList />
-          <MessageInput />
+          {account && <MessageInput />}
         </Window>
         <Thread />
       </Channel>
--- src/hooks/useClient.jsx
+++ src/hooks/useClient.jsx
@@ -2,32 +2,67 @@
 // or keep it in the `src/App.js`, up to you
 
 import { useEffect, useState } from "react";
-import { StreamChat } from "stream-chat";
+import { StreamChat, StreamType } from "stream-chat";
 
 // we'll use src/hooks/useClient.js path for this example
 export const useClient = ({ apiKey, userData, tokenOrProvider }) => {
   const [chatClient, setChatClient] = useState(null);
 
+  
   useEffect(() => {
-    const client = new StreamChat(apiKey);
-    // prevents application from setting stale client (user changed, for example)
-    let didUserConnectInterrupt = false;
+    if(tokenOrProvider && userData) {
+      let didUserConnectInterrupt = false;
+      console.log("useClient", userData)
+      const client = new StreamChat(apiKey);        
+      // const token = server.createToken('john');
+      // prevents application from setting stale client (user changed, for example)
 
-    const connectionPromise = client.connectUser(userData, tokenOrProvider).then(() => {
-      if (!didUserConnectInterrupt) setChatClient(client);
-    });
+      const connectionPromise = client.connectUser(userData, tokenOrProvider).then(() => {
+        if (!didUserConnectInterrupt) setChatClient(client);
+      });
+      // return () => {
+      //   didUserConnectInterrupt = true;
+      //   setChatClient(null);
+      //   // wait for connection to finish before initiating closing sequence
+      //   connectionPromise
+      //     .then(() => client.disconnectUser())
+      //     .then(() => {
+      //       console.log('connection closed');
+      //     });
+      // };
+    }
 
-    return () => {
-      didUserConnectInterrupt = true;
-      setChatClient(null);
-      // wait for connection to finish before initiating closing sequence
-      connectionPromise
-        .then(() => client.disconnectUser())
-        .then(() => {
-          console.log('connection closed');
-        });
-    };
-  }, [apiKey, userData.id, tokenOrProvider]);
+  }, [apiKey, userData, tokenOrProvider]);
 
   return chatClient;
 };
+
+export const fetchTokenFromApi = (name) => {
+  const [token, setToken] = useState(null)
+  useEffect(() => {
+    const api = async() => {
+      const res = await fetch("https://stream-server1.herokuapp.com/token", 
+      {
+        method: "post",
+        headers: { "Content-Type" : "application/json"},
+        body: JSON.stringify({
+          input: name
+        })
+      })
+
+      res.json().then((data) => {
+        setToken(data);
+      })
+    } 
+    // .then(response => response.text())
+    //   .then(result => {
+    //     console.log('token', result)
+    //     setToken(result)
+    //     // return result;
+    // })
+    // .catch(error => console.warn(error));
+    api();
+  }, [name])
+  return token;
+};
+
